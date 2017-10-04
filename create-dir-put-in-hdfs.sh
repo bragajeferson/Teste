80394548b65c64b7d1fe146c75538bbb43836ba1
Jeferson Braga (pragmatismo.io) <jeferson.braga@pragmatismo.io>
2017-10-04 01:25:21 -0300

    create shell script

--- create-dir-put-in-hdfs.sh
+++ create-dir-put-in-hdfs.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+#Carregar os arquivos vendas.txt e vendas_itens.txt para a pasta tmp
 hadoop fs -mkdir /user/mercafacil
 hadoop fs -put /tmp/vendas.txt /user/mercafacil
 hadoop fs -put /tmp/vendas_itens.txt /user/mercafacil
